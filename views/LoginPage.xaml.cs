e9b6e85546566a37a32e6bc257d3762980e57c40
Kayliese Smith <49692061+kayliese@users.noreply.github.com>
2022-05-09 09:03:24 -0400

    Update LoginPage.xaml.cs

--- views/LoginPage.xaml.cs
+++ views/LoginPage.xaml.cs
@@ -17,10 +17,6 @@ namespace spacex_explore.Views
 
             BindingContext = mainViewModel = new MainViewModel();
             commonService = new CommonService();
-
-            //testing purpose only
-            emailEnt.Text = "stephlowk@gmail.com";
-            passwordEnt.Text = "Stephanie121^";
         }
 
         async void signUpTap_Tapped(System.Object sender, System.EventArgs e)
@@ -30,28 +26,9 @@ namespace spacex_explore.Views
 
         async void signBtn_Clicked(System.Object sender, System.EventArgs e)
         {
-    /*        if (string.IsNullOrWhiteSpace(emailEnt.Text) || string.IsNullOrWhiteSpace(passwordEnt.Text))
-            {
-                await DisplayAlert("Error", "Please fill all the fields.", "OK");
-                return;
-            }
-
-            if (!commonService.IsValidEmail(emailEnt.Text))
-            {
-                await DisplayAlert("Error", "Please enter a valid email address.", "OK");
-                return;
-            }
             
-            var result = await mainViewModel.Login(new LoginModel { email = emailEnt.Text.Trim(), password = passwordEnt.Text.Trim()});
-    */
-          //  if (result)
-          //  {                
                 Application.Current.MainPage = new NavigationPage(new TabPage());
-          //  }
-          //  else
-           // {
-           //     await DisplayAlert("Login faild", "Please check your email and password again.", "OK");
-          //  }
+
         }
     }
 }
