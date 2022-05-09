c70d59bfc8fe030366aefc7468d50922734af28c
Kayliese Smith <49692061+kayliese@users.noreply.github.com>
2022-05-09 09:01:00 -0400

    Add files via upload

---
+++ views/HomePage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using spacex_explore.ViewModels;
+using Xamarin.Forms;
+
+namespace spacex_explore.Views
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
---
+++ views/LaunchesPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using spacex_explore.ViewModels;
+using Xamarin.Forms;
+
+namespace spacex_explore.Views
+{
+    public partial class LaunchesPage : ContentPage
+    {
+        public LaunchesPage()
+        {
+            InitializeComponent();
+        }       
+    }
+}
