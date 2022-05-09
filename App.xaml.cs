a5638b8bbdc8ebee353471f3a154cbc3224a03a6
Kayliese Smith <49692061+kayliese@users.noreply.github.com>
2022-05-09 09:10:02 -0400

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using spacex_explore.Views;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace spacex_explore
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new NavigationPage(new LoginPage());
+            Nav();
+
+            Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
+        }
+
+        private void Connectivity_ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+        {
+            Nav();
+        }
+
+        void Nav()
+        {
+            var current = Connectivity.NetworkAccess;
+
+            if (current == NetworkAccess.Internet)
+            { 
+                MainPage = new NavigationPage(new LoginPage());
+            }
+            else
+            {
+                MainPage = new NoInternetPage();
+            }
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Poppins-Regular.ttf", Alias = "Poppins")]
+[assembly: ExportFont("Poppins-Bold.ttf", Alias = "Poppins-Bold")]
+[assembly: ExportFont("Poppins-Light.ttf", Alias = "Poppins-Light")]
+[assembly: ExportFont("ionicons.ttf", Alias = "IonIcons")]
+
